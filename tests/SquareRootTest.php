204f6bdf0b68681e883d7f25209f79c4179de50a
itily <ilyha_roza@mail.ru>
2022-06-11 15:48:29 +0300

    HW3 First test

---
+++ tests/SquareRootTest.php
@@ -0,0 +1,28 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: itily
+ * Date: 11.06.2022
+ * Time: 15:06
+ */
+
+use PHPUnit\Framework\TestCase;
+
+/**
+ * Тест квадратного уравнения
+ *
+ * Class SquareRootTest
+ * @package ${NAMESPACE}
+ */
+class SquareRootTest extends TestCase
+{
+    /**
+     * Корней нет
+     *
+     * @return void
+     */
+    public function testShouldBeEmpty()
+    {
+        $this->markTestSkipped();
+    }
+}
